a351a697daf19d7dd76a065562d13064bd5d7db8
IlirImeri <ilirimeri2002@gmail.com>
2022-02-17 20:52:10 +0100

    Added google authentication, SignIn & SignOut functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { Dashboard } from './components/Dashboard.jsx';
+
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+const pingAPI = async () => {
+  let ping = await axios.get('https://api.coingecko.com/api/v3/ping');
+  console.log(ping);
+}
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+});
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Crypto Portfolio tracker</h1>
+        <SignOut/>
+       <section>
+         {user ? <Dashboard/> : <SignIn/>}
+         {console.log(process.env.DEV_API_KEY)}
+       </section>
       </header>
     </div>
   );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 export default App;
